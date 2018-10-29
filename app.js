926701d5f7565ee56c05be6ec9fc8132c3595397
aduxbury0 <duxburya@uni.coventry.ac.uk>
2018-10-29 15:25:30 +0000

    added routes for all needed pages

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser');
 
+
+//Import routes
+const routeLogin = require('./routes/login');
+const routeSignUp = require('./routes/sign-up');
+const routeUsers = require('./routes/user');
+
 //Initialising Express
 const app = express();
+
+//initialising routes
+app.use('/login', routeLogin);
+app.use('/signup', routeSignUp);
+app.use('/user', routeUsers);
 
 //Setting templating engine to Handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -11,6 +22,7 @@ app.set('view engine', 'handlebars');
 
 // Using Public as our static folder
 app.use(express.static('public'));
+
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
